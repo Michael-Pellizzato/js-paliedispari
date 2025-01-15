16927b5ced8c615cdd3bc8856186b11640f0a769
Michael-Pellizzato <pellizzato.michael97@gmail.com>
2025-01-15 16:15:47 +0100

    esercizio 1 e 2 bozza

---
+++ palindromi/main.js
@@ -0,0 +1,21 @@
+// Palidroma Chiedere all’utente di inserire una parola 
+// Creare una funzione per capire se la parola inserita è palindroma
+
+let parola = prompt("Inserisci la parola").toLowerCase();
+let alorap = ""; 
+
+
+function reverseString(){
+    return parola.split("").reverse("").join("") // [ "o", "s", "s", "o" ]
+}
+
+alorap = reverseString();
+
+
+if (parola === alorap){
+    console.log(La parola ${parola} é palindroma)
+} else {
+    console.log(La parola ${parola} non é palindroma)
+}
+
+reverseString(parola)
